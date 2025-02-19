15148441d74ee0b27691b1f4cc41c3f43af77296
Nikhil383 <nikhilmahesh89@gmail.com>
2025-02-20 00:51:05 +0530

    sixth commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.translation_model import TranslationModel
 
 app = Flask(__name__)
@@ -6,17 +6,19 @@ app = Flask(__name__)
 # Initialize translation model
 model = TranslationModel()
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        text = request.form["text"]
+        src_lang = request.form["src_lang"]
+        tgt_lang = request.form["tgt_lang"]
+        translation = model.translate(text, src_lang, tgt_lang)
+        return redirect(url_for("result", translation=translation))
     return render_template("index.html")
 
-@app.route("/translate", methods=["POST"])
-def translate():
-    text = request.form["text"]
-    src_lang = request.form["src_lang"]
-    tgt_lang = request.form["tgt_lang"]
-    translation = model.translate(text, src_lang, tgt_lang)
-    return render_template("index.html", translation=translation)
+@app.route("/result/<translation>")
+def result(translation):
+    return render_template("result.html", translation=translation)
 
 if __name__ == "__main__":
     app.run(debug=True)
